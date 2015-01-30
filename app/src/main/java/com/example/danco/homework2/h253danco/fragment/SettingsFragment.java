773546b3558960bceb2ac743fa7a9df73ccab9fc
costd035 <daniel.x.costinett.-nd@disney.com>
2015-01-30 10:27:39 -0800

    fix up settings implementation

--- app/src/main/java/com/example/danco/homework2/h253danco/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/danco/homework2/h253danco/fragment/SettingsFragment.java
@@ -1,20 +1,26 @@
 package com.example.danco.homework2.h253danco.fragment;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
+
+import com.example.danco.homework2.h253danco.R;
 
 /**
  * Created by danco on 1/27/15.
  */
-public class SettingsFragment extends Fragment {
+/**
+ * A simple {@link android.app.Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
 
-    public static SettingsFragment newInstance() {
-        return new SettingsFragment();
+    public SettingsFragment() {
+        // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
     }
 }
